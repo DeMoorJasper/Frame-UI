05bf3bce3f88aa3c0fe33e40235e7247382523b9
Jasper De Moor <jdemo@live.be>
2017-04-08 15:39:17 +0200

    require cleanup

--- js/FrameUI/init.js
+++ js/FrameUI/init.js
@@ -2,44 +2,47 @@ const ROOT_DIR = "./js/FrameUI/";
 /* THIS CONTAINS ALL USED CONTAINERS, ELEMENTS AND WINDOWS */
 let frameui = {};
 
-let loadUserElements = function() {
-  let userdir = "./js/HelloWorld/";
+require.config({
+    baseUrl: ROOT_DIR,
+    paths: {
+      /* Framework files - Dependancies */
+      'FRAMEUIproperty': 'property',
+      'FRAMEUIproperties': 'properties',
+      'FRAMEUIcontainer': 'container',
+      'FRAMEUIelement': 'element',
+      'FRAMEUIwindow': 'uiwindow',
+      /* User defined elements, containers and windows */
+      'main': '../HelloWorld/windows/main',
+      'block': '../HelloWorld/containers/block',
+      'nav': '../HelloWorld/containers/nav',
+      'navitem': '../HelloWorld/elements/navitem',
+      'ul': '../HelloWorld/containers/ul',
+      'h1': '../HelloWorld/elements/h1',
+      /* init script */
+      'example': '../HelloWorld/example',
+    }
+});
 
-  /* EXECUTE THIS AT THE END */
+let loadUserElements = function() {
+  /* UPDATE THIS WITH ALL NEW ELEMENTS YOU ADD */
+  let userscripts = ['main', 'block', 'nav', 'navitem', 'ul', 'h1'];
+  /* This script will be executed once all dependancies and elements all loaded */
+  let initscript = "example";
+
   let start = function() {
-    require([userdir + 'example'], function() {
+    require([initscript], function() {
       init();
     });
   };
-  /* ==================== */
-
-  require([userdir + 'windows/main'], function() {
-    require([userdir + 'containers/block'], function() {
-      require([userdir + 'containers/nav'], function() {
-        require([userdir + 'elements/navitem'], function() {
-          require([userdir + 'containers/ul'], function() {
-            require([userdir + 'elements/h1'], function() {
-              start();
-            });
-          });
-        });
-      });
-    });
+  require(userscripts, function() {
+    start();
   });
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
   /* LOADS ALL CORE OBJECTS */
-  require([ROOT_DIR + 'property'], function() {
-    require([ROOT_DIR + 'properties'], function() {
-      require([ROOT_DIR + 'container'], function() {
-        require([ROOT_DIR + 'element'], function() {
-          require([ROOT_DIR + 'uiwindow'], function() {
-            /* LOAD USER DEFINED OBJECTS */
-            loadUserElements();
-          });
-        });
-      });
-    });
+  require(['FRAMEUIproperty', 'FRAMEUIproperties', 'FRAMEUIcontainer', 'FRAMEUIelement', 'FRAMEUIwindow'], function() {
+    /* LOAD USER DEFINED OBJECTS */
+    loadUserElements();
   });
 });
